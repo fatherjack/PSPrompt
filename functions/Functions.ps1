1e79fe785dc807922ea68d2880c7910435cb512e
fatherjack <jonathanallen69@gmail.com>
2019-04-15 15:52:29 +0100

    working function and aliases

--- functions/Functions.ps1
+++ functions/Functions.ps1
@@ -1,16 +1,15 @@
 #region search engine functions
 function New-WebSearch {
     param(
-        [parameter()][string]$engine,
         [parameter()][string]$search
     )
     process {
-        Write-Host (Get-PSCallStack).InvocationInfo.MyCommand.Definition -ForegroundColor Red -BackgroundColor Yellow
+        $engine = (Get-PSCallStack).InvocationInfo.MyCommand.Definition[1] # -ForegroundColor Red -BackgroundColor Yellow
         switch -Regex ($engine) {
-            'DDG | DuckDuckGo' { $url = "https://duckduckgo.com/?q=$search" }
-            'Google' { $url = "https://www.google.co.uk/search?q=$Search" }
-            'Bing' { $url = "https://www.bing.com/search?q=$Search" }
-            'Ask' { $url = "https://uk.ask.com/web?q=$Search" }
+            {$_ -match ('DDG | DuckDuckGo')} { $url = "https://duckduckgo.com/?q=$search";break }
+            {$_ -match 'Google'} { $url = "https://www.google.co.uk/search?q=$Search";break }
+            {$_ -match 'Bing'} { $url = "https://www.bing.com/search?q=$Search";break }
+            {$_ -match 'Ask'} { $url = "https://uk.ask.com/web?q=$Search";break }
             default { $url = "https://www.bing.com/search?q=$Search" }
         }    
         Start-Process $url
@@ -18,19 +17,12 @@ function New-WebSearch {
 }
 #endregion
 
-#region specific engine functions
-# function Google { param($String) New-WebSearch -engine 'Google' -search $String }
-# function DuckDuckGo { param($String) New-WebSearch -engine 'DuckDuckGo' -search $String }
-# function Bing { param($String) New-WebSearch -engine 'Bing' -search $String }
-# function Ask { param($String) New-WebSearch -engine 'Google' -search $String }
-#endregion
-
 #region Search aliases
-
 set-Alias -Name Google -Value New-WebSearch 
 set-Alias -Name DuckDuckGo -Value New-WebSearch  
-set-Alias -Name DDG -Value New-WebSearch  
-set-Alias -Name Ask -Value New-WebSearch 
+Set-Alias -Name DDG -Value New-WebSearch  
+Set-Alias -Name Ask -Value New-WebSearch  
+Set-Alias -Name Bing -Value New-WebSearch 
 #endregion
 
 #endregion

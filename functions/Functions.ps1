e218f0211d2bb43a7ae8da1a1f0eae12792d242d
fatherjack <jonathanallen69@gmail.com>
2019-04-15 16:00:34 +0100

    corrected psd function name + added missing alias

--- functions/Functions.ps1
+++ functions/Functions.ps1
@@ -1,15 +1,16 @@
 #region search engine functions
 function New-WebSearch {
     param(
+        # the search string
         [parameter()][string]$search
     )
     process {
         $engine = (Get-PSCallStack).InvocationInfo.MyCommand.Definition[1] # -ForegroundColor Red -BackgroundColor Yellow
         switch -Regex ($engine) {
-            {$_ -match ('DDG | DuckDuckGo')} { $url = "https://duckduckgo.com/?q=$search";break }
-            {$_ -match 'Google'} { $url = "https://www.google.co.uk/search?q=$Search";break }
-            {$_ -match 'Bing'} { $url = "https://www.bing.com/search?q=$Search";break }
-            {$_ -match 'Ask'} { $url = "https://uk.ask.com/web?q=$Search";break }
+            { $_ -match ('DDG | DuckDuckGo') } { $url = "https://duckduckgo.com/?q=$search"; break }
+            { $_ -match 'Google' } { $url = "https://www.google.co.uk/search?q=$Search"; break }
+            { $_ -match 'Bing' } { $url = "https://www.bing.com/search?q=$Search"; break }
+            { $_ -match 'Ask' } { $url = "https://uk.ask.com/web?q=$Search"; break }
             default { $url = "https://www.bing.com/search?q=$Search" }
         }    
         Start-Process $url

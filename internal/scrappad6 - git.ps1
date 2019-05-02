864f4d99243e5c5ae7fd3683332e9d7072700aef
fatherjack <jonathanallen69@gmail.com>
2019-05-02 22:47:12 +0200

    ready for final testing before going to prompt

--- internal/scrappad6 - git.ps1
+++ internal/scrappad6 - git.ps1
@@ -1,15 +1,19 @@
-$status = git status
 
-if ($status[0] -eq "On branch master"){
-    write-host "WARNING YOU ARE EDITING MASTER. STOP IT. YOU SHOULD BRANCH BEFORE YOU EDIT" -ForegroundColor Yellow -BackgroundColor Red
+# logic to find every nth execution 
+if ((Get-History -Count 1).ID % 5 -eq 0) {
+    $status = git status
+
+    if ($status[0] -eq "On branch master") {
+        write-host "WARNING YOU ARE EDITING MASTER. STOP IT. YOU SHOULD BRANCH BEFORE YOU EDIT" -ForegroundColor Yellow -BackgroundColor Red
+    }
+
+    $NewFiles = $status -match "New:"
+    $ModFiles = $status -match "modified:"
+    $DelFiles = $status -match "deleted:"
+
+    write-host "Git: "      -NoNewline
+    write-host "New $($NewFiles.Count); " -NoNewline
+    write-host "Mod $($ModFiles.Count); " -NoNewline
+    write-host "Del $($DelFiles.Count)"   
+    
 }
-
-$NewFiles = $status -match "New:"
-$ModFiles = $status -match "modified:"
-$DelFiles = $status -match "deleted:"
-
-write-host "Git: "      -NoNewline
-write-host "New {0}; " -NoNewline
-write-host "Mod {1}; " -NoNewline
-write-host "Del {2}"   -NoNewline
--f $NewFiles.Count, $ModFiles.Count, $DelFiles.Count 

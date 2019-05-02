454839b288fa9a251dad2062223250d1421fd332
fatherjack <jonathanallen69@gmail.com>
2019-05-02 16:21:57 +0200

    Git status on every 5th command

--- internal/git_status.ps1
+++ internal/git_status.ps1
@@ -1,14 +1,16 @@
-Set-Location C:\Users\jonallen\OneDrive\Documents\GitHub\HowToGit\
-$r = git status
-$NewFiles = ($r -match 'new file:').count
-$ModFiles = ($r -match 'modified:').count
-$DelFiles = ($r -match 'deleted:').count
-$Branch = $r[0]
+#Set-Location C:\Users\jonallen\OneDrive\Documents\GitHub\HowToGit\
 
-Write-Host "git status: " -ForegroundColor White -BackgroundColor Black -NoNewline
-Write-Host "New[$NewFiles] " -ForegroundColor Green -BackgroundColor Black -NoNewline
-Write-Host "Mod[$ModFiles] " -ForegroundColor DarkCyan -BackgroundColor Black -NoNewline
-Write-Host "Del[$DelFiles] " -ForegroundColor Red -BackgroundColor Black -NoNewline
-if ($Branch -eq "On branch master") { Write-Host "WARNING : Working in MASTER" -ForegroundColor Red -BackgroundColor Black }
-else { Write-Host "$r[0]" -ForegroundColor Blue -BackgroundColor Black }
+if ((Get-History -Count 1).ID % 5 -eq 0) {
+    $r = git status
+    $NewFiles = ($r -match 'new file:').count
+    $ModFiles = ($r -match 'modified:').count
+    $DelFiles = ($r -match 'deleted:').count
+    $Branch = $r[0]
 
+    Write-Host "git status: " -ForegroundColor White -BackgroundColor Black -NoNewline
+    if ($Branch -eq "On branch master") { Write-Host "*WARNING : Working in MASTER.*" -ForegroundColor Yellow -BackgroundColor Red -NoNewline } 
+    else { Write-Host "$r[0]" -ForegroundColor Blue -BackgroundColor Black -NoNewline } 
+    Write-Host " New[$NewFiles] " -ForegroundColor Green -BackgroundColor Black -NoNewline
+    Write-Host " Mod[$ModFiles] " -ForegroundColor DarkCyan -BackgroundColor Black -NoNewline
+    Write-Host " Del[$DelFiles] " -ForegroundColor Red -BackgroundColor Black 
+}

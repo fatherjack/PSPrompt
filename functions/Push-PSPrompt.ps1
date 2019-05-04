f8597bfc03637ca1266a63ecc619382469676ad4
fatherjack <jonathanallen69@gmail.com>
2019-05-04 09:36:01 +0200

    something very wrong in lines 24 - 31.

--- functions/Push-PSPrompt.ps1
+++ functions/Push-PSPrompt.ps1
@@ -19,52 +19,66 @@ function Push-PSPrompt {
     #     [parameter()]$PSPromptData
     # )
     #region build up script from components
-    $PromptFile = "$WorkingFolder\MyPrompt.ps1"
-    $ModulePath = ($env:PSModulePath -split (';'))[1]
+    #begin {
+        New-Variable -Name WorkingFolder -Value "$env:APPDATA\PSPrompt" -Option Constant
+        $PromptFile = "$WorkingFolder\MyPrompt.ps1"
+        $ModulePath = ($env:PSModulePath -split (';'))[1]
+        $mpath = (get-module -name psprompt).path
+        $Path = split-path $mpath -parent
+        $child = "\functions\components"
+        write-verbose $path
+        write-verbose $child
+        $components = (Join-Path -path $Path -ChildPath $child)
 
-    ####Write-Warning $components
+    #}
+    #process { 
 
-    # step one - the start of a function boiler-plate
-    get-content "$components\_header.txt" | Out-File $PromptFile -Force
+        ####Write-Warning $components
 
-    # read in the settings from the config file created in Set-PSPrompt
-    if (test-path "$WorkingFolder\PSPrompt.config" ) {
-        $PSPromptData = import-Clixml -Path "$WorkingFolder\PSPrompt.config" 
-    }
-    else {
-        $msg = "Unable to read config file at $WorkingFolder\PSPrompt.config, check that it exists and then run Set-PSPrompt. "
-        Write-Warning $msg
-    }
+        # step one - the start of a function boiler-plate
+        get-content "$components\_header.txt" | Out-File $PromptFile -Force
 
-    # now for each value from our hash table we need to gather the script component
-    # firstly - components in the permanent prompt line 
-    switch ($PSPromptData) {
-        { $_.Admin } { get-content "$components\admin.txt" | Out-File $PromptFile -Append }
-        { $_.Battery } { get-content "$components\battery.txt" | Out-File $PromptFile -Append }
-        { $_.Day_and_date } { get-content "$components\daydate.txt" | Out-File $PromptFile -Append }
-        { $_.UTC_offset } { get-content "$components\UTC_offset.txt" | Out-File $PromptFile -Append }
-        { $_.last_command } { get-content "$components\last_command.txt" | Out-File $PromptFile -Append }
-        { $_.shortpath } { get-content "$components\shortpath.txt" | Out-File $PromptFile -Append }
-    }
-    # then the second prompt line that is shown occasionally
-    If ($PSPromptData.SecondLine) {
+        # read in the settings from the config file created in Set-PSPrompt
+        if (test-path "$WorkingFolder\PSPrompt.config" ) {
+            $PSPromptData = import-Clixml -Path "$WorkingFolder\PSPrompt.config" 
+        }
+        else {
+            $msg = "Unable to read config file at $WorkingFolder\PSPrompt.config, check that it exists and then run Set-PSPrompt. "
+            Write-Warning $msg
+        }
+
+        # now for each value from our hash table we need to gather the script component
+        # firstly - components in the permanent prompt line 
         switch ($PSPromptData) {
-            { $_.GitStatus } { get-content "$components\shortpath.txt" | Out-File $PromptFile -Append })
-    }
+            { $_.Admin } { get-content "$components\admin.txt" | Out-File $PromptFile -Append }
+            { $_.Battery } { get-content "$components\battery.txt" | Out-File $PromptFile -Append }
+            { $_.Day_and_date } { get-content "$components\daydate.txt" | Out-File $PromptFile -Append }
+            { $_.UTC_offset } { get-content "$components\UTC_offset.txt" | Out-File $PromptFile -Append }
+            { $_.last_command } { get-content "$components\last_command.txt" | Out-File $PromptFile -Append }
+            { $_.shortpath } { get-content "$components\shortpath.txt" | Out-File $PromptFile -Append }
+        }
+        # then the second prompt line that is shown occasionally
+        If ($PSPromptData.SecondLine) {
+            switch ($PSPromptData) {
+                { $_.GitStatus } { get-content "$components\shortpath.txt" | Out-File $PromptFile -Append }
+            }
 
 
-    # complete the Prompt function in the file so that we can dot source it dreckly
-    get-content "$componentspath\_footer.txt" | Out-File $PromptFile -Append
-    write-verbose $PromptFile
+            # complete the Prompt function in the file so that we can dot source it dreckly
+            get-content "$componentspath\_footer.txt" | Out-File $PromptFile -Append
+            write-verbose $PromptFile
 
-    # dot source the prompt to apply the changes
-    try {  
-        . $PromptFile
-        write-host "`r`nCongratulations!! `r`nYour prompt has been updated. If you want to change the components in effect, just run Set-PSPrompt again. 
+            # dot source the prompt to apply the changes
+            try {  
+                . $PromptFile
+                write-host "`r`nCongratulations!! `r`nYour prompt has been updated. If you want to change the components in effect, just run Set-PSPrompt again. 
         `r`nIf you want to remove the PSPrompt changes run Set-PSPrompt -reset`r`n"
-    }    
-    catch {
-        Write-Warning "Something went wrong with applying the PSPrompt changes." 
-    }
-    #endregion
+            }    
+            catch {
+                Write-Warning "Something went wrong with applying the PSPrompt changes." 
+            }
+            #endregion
+        }
+    #}
+    #end { }
 }

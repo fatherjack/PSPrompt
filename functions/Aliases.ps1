cee034d29f91f460447bce0524628d83267e38f0
fatherjack <jonathanallen69@gmail.com>
2019-05-23 14:07:19 -0400

    aliascsetting moved to functions. commented out here

--- functions/Aliases.ps1
+++ functions/Aliases.ps1
@@ -1,10 +1,12 @@
-#region Search aliases
-set-Alias -Name Google -Value New-WebSearch 
-set-Alias -Name DuckDuckGo -Value New-WebSearch  
-Set-Alias -Name DDG -Value New-WebSearch  
-Set-Alias -Name Ask -Value New-WebSearch  
-Set-Alias -Name Bing -Value New-WebSearch 
-Set-Alias -Name Cal -Value Get-OutlookCalendar
-Set-Alias -Name Events -Value Get-OutlookCalendar 
-New-Alias -Name Dim -Value Set-DisplayBrightness
-#endregion
+# #region Search aliases
+# set-Alias -Name Google -Value New-WebSearch 
+# set-Alias -Name DuckDuckGo -Value New-WebSearch  
+# Set-Alias -Name DDG -Value New-WebSearch  
+# Set-Alias -Name Ask -Value New-WebSearch  
+# Set-Alias -Name Bing -Value New-WebSearch 
+
+# Set-Alias -Name Cal -Value Get-OutlookCalendar
+# Set-Alias -Name Events -Value Get-OutlookCalendar 
+
+# New-Alias -Name Dim -Value Set-DisplayBrightness
+# #endregion

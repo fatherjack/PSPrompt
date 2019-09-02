35dcd3f0da19035ed10a3b3428f1cf09e7d60eae
fatherjack <jonathanallen69@gmail.com>
2019-09-02 22:58:38 +0100

    1 file 1 function

---
+++ PSPrompt/functions/ConvertTo-Epoch.ps1
@@ -0,0 +1,23 @@
+﻿function ConvertTo-Epoch {
+    <#
+    .SYNOPSIS
+    Converts a datetime to epoch int value
+
+    .DESCRIPTION
+    Converts a value like 10-Apr-2018 10:45:01 to an epoch style int value
+
+    .PARAMETER date
+    The date value that should be converted
+
+    .EXAMPLE
+    ConvertTo-Epoch '10-Apr-2015 12:30:00' 
+
+#>
+    [cmdletbinding()]
+    param(
+        # date value to convert to epoch
+        [parameter(ValueFromPipeline = $true, Mandatory = $true)][datetime]
+        $date
+    )
+    [int][double]::Parse((Get-Date ($date).ToUniversalTime() -UFormat %s))
+}

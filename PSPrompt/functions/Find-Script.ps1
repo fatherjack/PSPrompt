a7b9eba3fb4d5aaec9d59e26fed9cec3f9a490b3
Fatherjack <jonathanallen69@outlook.com>
2019-11-03 17:33:27 +0000

    adding new parameters Type and Location

--- PSPrompt/functions/Find-Script.ps1
+++ PSPrompt/functions/Find-Script.ps1
@@ -24,15 +24,23 @@
                 
     param (
         # The string you want to search for
-        [parameter(Mandatory = $false)]$Search,
+        [parameter(Mandatory = $true)]
+        [ValidateScript]
+        [string]$Search,
         # The file type you want to search for
-        [parameter(Mandatory = $false)]$Type,
+        [parameter(Mandatory = $false)]
+        [string]$Type,
+        # custom locations to search
+        [parameter(Mandatory = $false)]
+        [string]$Locations,
         # Do you want to search in file content too
-        [parameter(Mandatory = $false)][switch]$IncludeContent
+        [parameter(Mandatory = $false)]
+        [switch]$IncludeContent
     )
       
     $Type = '*.' + $Type
     $List = Import-Csv 'C:\Users\jonallen\OneDrive\PowerShellLocations.csv'
+    if ($Locations) { $List += $Locations -split ',' }
   
     # $Results = [[PSCustomObject]@ {
     #     MatchType = $null
@@ -40,8 +48,9 @@
     #     FilePath = $null
     # }]
     $Results = ForEach ($Item in $List) {
-    
-        ".. Checking $($item.name) .."
+        $msg = if ($null -eq $item.name){$Item.folder}else{$item.name}
+        Write-Output ".. Checking $msg .."
+
         foreach ($doc in Get-ChildItem $item.Folder -Recurse -include $Type) {
             if ($doc -match $search) {
                 [pscustomobject]@{
